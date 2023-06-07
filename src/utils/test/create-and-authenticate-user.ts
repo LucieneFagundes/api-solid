366c3906993f798356813e68402d222cb6f3f1f1
LucieneFagundes <lfnascimento99@gmail.com>
2023-06-07 10:55:06 -0300

    :sparkles: feat: RBAC -> implemented and tested

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,11 +1,19 @@
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
+import { prisma } from 'src/lib/prisma'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post('/users').send({
-    name: 'Jane Doe',
-    email: 'jdoe@example.com',
-    password: '123456',
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'Jane Doe',
+      email: 'jdoe@example.com',
+      password_hash: await hash('123456', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
